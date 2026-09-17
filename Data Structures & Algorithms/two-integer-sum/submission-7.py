1296964012730964a11f2c255f1a17ec8fc001cb
Hope Tan <hopedtan@gmail.com>
2026-09-17 13:09:29 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        # see if we've seen a value that is target-value before
+        # hashmap gives O(1) lookups
+        # add values to hashmap each iteration and check target-i
+        check = {}
+        for index, value in enumerate(nums):
+            if (target - value) in check:
+                if index < check[target - value]:
+                    return [index, check[target - value]]
+                return [check[target - value], index]
+            check[value] = index
+        return []
